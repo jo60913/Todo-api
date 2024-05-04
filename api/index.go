346107ae56036e1c45a966b2c3c74500de5cc354
huangweizhe <naruto60913@gmail.com>
2024-05-04 23:08:25 +0800

    簡化輸出

--- api/index.go
+++ api/index.go
@@ -1,17 +1,10 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func Handler(r *gin.Engine) {
-	api := r.Group("/api")
-	api.GET("/get", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"ErrorMsg":  "欄位錯誤",
-			"ErrorFlag": "3",
-		})
-	})
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
