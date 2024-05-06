a9903c23d33d5bbbe9991e4f570bf164596f5aae
huangweizhe <naruto60913@gmail.com>
2024-05-06 20:44:20 +0800

    修改為gin做api

--- api/index.go
+++ api/index.go
@@ -1,45 +1,38 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 
-	. "github.com/tbxark/g4vercel"
+	"github.com/gin-gonic/gin"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "hellow",
+		})
+	})
 
-	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello go from vercel !!!!",
-		})
-	})
-	server.POST("/hello", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
+	router.POST("/hello", func(c *gin.Context) {
+		var notificationUpdate NotificationUpdate
+		err := c.ShouldBindJSON(&notificationUpdate)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"ErrorMsg":  "欄位錯誤",
+				"ErrorFlag": "3",
 			})
-		} else {
-			context.JSON(200, H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
+			return
 		}
-	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
+		c.JSON(http.StatusOK, gin.H{
+			"ErrorMsg":  notificationUpdate.NotificationValue,
+			"ErrorFlag": "0",
 		})
 	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
-		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
-		})
-	})
-	server.Handle(w, r)
+
+}
+
+type NotificationUpdate struct {
+	UserToken         string `json:"UserToken"`
+	NotificationValue bool   `json:"NotificationValue"`
 }
